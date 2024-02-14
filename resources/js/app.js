417e174a3ffc62ba3f16fc37f9f565cb3d3d2c5e
C3Computer007 <151006478+C3Computer007@users.noreply.github.com>
2024-02-14 22:00:51 +0630

    bootstrap,modal, sweetalert2,scroll reveal

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,60 @@
-require('./bootstrap');
+// require('./bootstrap');
+
+import ScrollReveal from "scrollreveal";
+
+import Swal from "sweetalert2";
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
+
+let showModel = document.getElementById("showModel");
+showModel.addEventListener("click", function () {
+    // console.log("San kyi tar par");
+
+    let btnModel = new bootstrap.Modal(
+        document.getElementById("btnModel", {
+            backdrop: "static",
+        })
+    );
+    btnModel.show();
+});
+
+let checkbtn = document.getElementById("checkbtn");
+checkbtn.addEventListener("click", function () {
+    // console.log("checking");
+    // alert("are you suer");
+    // confirm("are you sure delete?");
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "question",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+        if (result.isConfirmed) {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                },
+            });
+            Toast.fire({
+                icon: "success",
+                title: "Signed in successfully",
+            });
+        }
+    });
+});
+
+ScrollReveal().reveal(".list-group-item", {
+    distance: "30px",
+    origin: "top",
+    duration: 500,
+    interval: 200,
+});
